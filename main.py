904e3e29b2e431fc0a16214d287e991a07797bf7
Ice-Smith303 <ice.smith9457@gmail.com>
2023-06-15 13:54:24 +1200

    Commit 2 - Initialisng Window

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #91906 2nd attempt
+
+import tkinter as tk
+from tkinter import messagebox
+
+class Calculator:
+    def __init__(self, root):
+        pass
+
+
+root = tk.Tk()
+calculator = Calculator(root)
+root.mainloop()
+
