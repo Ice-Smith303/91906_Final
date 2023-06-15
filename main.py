04a89204de0cb74572325debd653fe8cb28a77cc
Ice-Smith303 <ice.smith9457@gmail.com>
2023-06-15 20:01:42 +1200

    Commit 4 - Writing buttons with more efficient and clean code

--- main.py
+++ main.py
@@ -11,37 +11,31 @@ class Calculator:
         self.root.resizable(1, 1)
 
         # Entry Field
-        self.entry_field = tk.Entry(self.root, font=("Roboto", 20), justify="center")
-        self.entry_field.grid(row=0, column=0, sticky="n")
+        self.entry_field = tk.Entry(self.root, font=("Helvetica", 20), justify="right")
+        self.entry_field.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
         # Buttons
-        column1x = 40
-        column2x = 105
-        column3x = 170
-        column4x = 235
+        buttons = [
+            "7", "8", "9", "/",
+            "4", "5", "6", "*",
+            "1", "2", "3", "-",
+            "0", ".", "=", "+"
+        ]
 
-        row1y = 100
-        row2y = 160
-        row3y = 220
-        row4y = 280
+        row = 1
+        col = 0
+        for button in buttons:
+            btn = tk.Button(self.root, text=button, width=5, height=2)
+            btn.grid(row=row, column=col, padx=5, pady=5)
+            btn.bind("<Button-1>")
+            col += 1
+            if col > 3:
+                col = 0
+                row += 1
 
-        # num column 1
-        number7 = tk.Button(root, text='7', font='broadway 32', bg='yellow',
-                         height=1, width=1)
-        canvas.create_window(column1x, row1y, window=number7)
-
-        number4 = tk.Button(root, text='4', font='broadway 32', bg='yellow',
-                         height=1, width=1)
-        canvas.create_window(column1x, row2y, window=number4)
-
-        number1 = tk.Button(root, text='1', font='broadway 32', bg='yellow',
-                         height=1, width=1)
-        canvas.create_window(column1x, row3y, window=number1)
 
 
 root = tk.Tk()
-canvas = tk.Canvas(root, width=273, height=400, bg='lightblue')
-canvas.grid()
 calculator = Calculator(root)
 root.mainloop()
 
