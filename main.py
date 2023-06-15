5a0f2677f1416c139ae832ccd9ce744fd313fc83
Ice-Smith303 <ice.smith9457@gmail.com>
2023-06-16 09:11:06 +1200

    Commit 5 - setting the layout for buttons and creating frames for display and buttons

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 #91906 2nd attempt
 
+import customtkinter as ctk
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import *
 
+
+LIGHT_GRAY = "#F5F5F5"
 class Calculator:
     def __init__(self, root):
         self.root = root
         self.root.title("Calculator")
-        self.root.geometry("365x300")
+        self.root.geometry("400x600")
         self.root.resizable(1, 1)
+        self.root.configure(background="white")
+
+        self.total_expression = "0"
+        self.current_expression = "0"
+
+        self.display_frame = self.create_display_frame()
+        self.buttons_frame = self.create_buttons_frame()
+
+    def create_display_frame(self):
+        frame=tk.Frame(self.root, height=221, bg="blue")
+        frame.grid(columnspan=2, sticky="nsew")
+
+
+    def create_buttons_frame(self):
+        frame=tk.Frame(self.root)
+        frame.grid(sticky="nsew", columnspan=2)
+
+
+
+
 
         # Entry Field
         self.entry_field = tk.Entry(self.root, font=("Helvetica", 20), justify="right")
@@ -28,10 +51,14 @@ class Calculator:
             btn = tk.Button(self.root, text=button, width=5, height=2)
             btn.grid(row=row, column=col, padx=5, pady=5)
             btn.bind("<Button-1>")
+            root.grid_columnconfigure(col, weight=1)
+            root.grid_rowconfigure(row, weight=1)
             col += 1
             if col > 3:
                 col = 0
                 row += 1
+
+
 
 
 
