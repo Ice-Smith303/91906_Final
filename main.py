8ce2f6d54b0101dc16ad6b26e75ab0124af3fc61
Ice-Smith303 <ice.smith9457@gmail.com>
2023-06-16 09:46:11 +1200

    Commit 6

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import customtkinter as ctk
 import tkinter as tk
 from tkinter import *
 
-
+WHITE = "#FFFFFF"
+LARGE_FONT = ("Arial", 40, "bold")
+SMALL_FONT = ("Arial", 16)
+DIGITS_FONT = ("Arial", 24, "bold")
+LABEL_COLOUR = "#25265E"
 LIGHT_GRAY = "#F5F5F5"
 class Calculator:
     def __init__(self, root):
@@ -14,21 +18,46 @@ class Calculator:
         self.root.resizable(1, 1)
         self.root.configure(background="white")
 
-        self.total_expression = "0"
+        self.sum_expression = "0"
         self.current_expression = "0"
+        self.display_frame=self.create_display_frame()
+
+        self.sum_label, self.label = self.create_display_labels()
 
         self.display_frame = self.create_display_frame()
         self.buttons_frame = self.create_buttons_frame()
 
+        self.digits = {
+            7: (1, 1), 8: (1, 2), 9: (1, 3),
+            4: (2, 1), 5: (2, 2), 6: (2, 3),
+            1: (3, 1), 2: (3, 2), 3: (3, 3),
+            0: (4, 2), '.': (4, 1)
+        }
+        self.operations = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
+
+        self.create_digit_buttons()
+
+
     def create_display_frame(self):
         frame=tk.Frame(self.root, height=221, bg="blue")
         frame.grid(columnspan=2, sticky="nsew")
-
+        return(frame)
 
     def create_buttons_frame(self):
         frame=tk.Frame(self.root)
         frame.grid(sticky="nsew", columnspan=2)
+        return(frame)
 
+    def create_display_labels(self):
+        sum_label = tk.Label(self.display_frame, text=self.sum_expression,
+                             bg=LIGHT_GRAY, fg=LABEL_COLOUR,padx=24, font=SMALL_FONT)
+        sum_label.grid(sticky="nsew")
+
+        label = tk.Label(self.display_frame, text=self.current_expression,
+                             bg=LIGHT_GRAY, fg=LABEL_COLOUR,padx=24, font=LARGE_FONT)
+        label.grid(sticky="nsew")
+
+        return(sum_label,label)
 
 
 
@@ -38,26 +67,12 @@ class Calculator:
         self.entry_field.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
         # Buttons
-        buttons = [
-            "7", "8", "9", "/",
-            "4", "5", "6", "*",
-            "1", "2", "3", "-",
-            "0", ".", "=", "+"
-        ]
 
-        row = 1
-        col = 0
-        for button in buttons:
-            btn = tk.Button(self.root, text=button, width=5, height=2)
-            btn.grid(row=row, column=col, padx=5, pady=5)
-            btn.bind("<Button-1>")
-            root.grid_columnconfigure(col, weight=1)
-            root.grid_rowconfigure(row, weight=1)
-            col += 1
-            if col > 3:
-                col = 0
-                row += 1
-
+    def create_digit_buttons(self):
+        for digit, grid_value in self.digits.items():
+            button = tk.Button(self.buttons_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOUR, font=DIGITS_FONT,
+                               borderwidth=0, command=lambda x=digit: self.add_to_expression(x))
+            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
 
 
 
