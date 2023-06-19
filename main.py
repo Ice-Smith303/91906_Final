1f341a22258246398fe23ecf3bb0caed28ed81bc
Ice-Smith303 <ice.smith9457@gmail.com>
2023-06-19 12:12:00 +1200

    Commit 11 - Adding button functionality, buttons now display expressions on the expression label and sum label. Still need to create a method that calculates the answer and displays it.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-#91906 Calculator Ice Smith 2023
+# 91906 Calculator Ice Smith 2023
 
 import tkinter as tk
-from tkinter import *
-
-
 
 # Defining Constants that define font, size and weight.
 DEFAULT_FONT_STYLE = ("Arial", 24)
@@ -16,19 +13,21 @@ CREAM = "#F8FAFF"
 LABEL_COLOUR = "#25265E"
 LIGHT_GRAY = "#F5F5F5"
 LIGHT_BLUE = "#CCEDFF"
+
+
 class Calculator:
     def __init__(self, root):
-        # initialsing and defining window attributes
+        # initializing and defining window attributes
         self.root = root
         self.root.title("Calculator")
         self.root.geometry("300x600")
         self.root.resizable(1, 1)
         self.root.configure(background="white")
-        root.attributes('-alpha', 0.92)
+        root.attributes('-alpha', 0.94)
 
         # defining data structures
-        self.sum_expression = "0"
-        self.current_expression = "0"
+        self.sum_expression = ""
+        self.current_expression = ""
         self.digits = {
             7: (1, 1), 8: (1, 2), 9: (1, 3),
             4: (2, 1), 5: (2, 2), 6: (2, 3),
@@ -48,10 +47,30 @@ class Calculator:
 
         # Assigning weight to buttons within the buttons_frame frame so that they expand to frame size.
         self.buttons_frame.rowconfigure(0, weight=1)
-        for x in range(1,5):
+        for x in range(1, 5):
             self.buttons_frame.rowconfigure(x, weight=1)
             self.buttons_frame.columnconfigure(x, weight=1)
 
+    # creates display_frame that will hold display labels.
+    def create_display_frame(self):
+        frame = tk.Frame(self.root, bg=LIGHT_GRAY)
+        frame.grid(columnspan=4, sticky="nsew")
+        return frame
+
+    # Creates buttons_frame that will hold buttons.
+    def create_buttons_frame(self):
+        frame = tk.Frame(self.root, bg="green")
+        frame.grid(sticky="nsew")
+        self.display_frame.columnconfigure(0, weight=1)
+        self.display_frame.rowconfigure(0, weight=1)
+        self.display_frame.rowconfigure(1, weight=1)
+        return frame
+
+    # calling method that calls the class methods that create the calculator frames and labels.
+    def create_frames_labels(self):
+        self.display_frame = self.create_display_frame()
+        self.sum_label, self.label = self.create_display_labels()
+        self.buttons_frame = self.create_buttons_frame()
 
     # calling method that calls the class methods that create the calculator buttons.
     def create_buttons(self):
@@ -60,45 +79,27 @@ class Calculator:
         self.create_clear_button()
         self.create_equals_button()
 
-    # calling method that calls the class methods that create the calculator frames and labels.
-    def create_frames_labels(self):
-        self.display_frame=self.create_display_frame()
-        self.sum_label, self.label = self.create_display_labels()
-        self.buttons_frame = self.create_buttons_frame()
-
-    # creates display_frame that will hold display labels.
-    def create_display_frame(self):
-        frame=tk.Frame(self.root, bg=LIGHT_GRAY)
-        frame.grid(columnspan=4, sticky="nsew")
-        return(frame)
-
-    # Creates buttons_frame that will hold buttons.
-    def create_buttons_frame(self):
-        frame=tk.Frame(self.root, bg="green")
-        frame.grid(sticky="nsew")
-        self.display_frame.columnconfigure(0, weight=1)
-        self.display_frame.rowconfigure(0, weight=1)
-        self.display_frame.rowconfigure(1, weight=1)
-        return(frame)
+    def add_to_label(self, value):
+        self.current_expression += str(value)
+        self.update_label()
 
     # Creates display labels that display the summation and the expression on seperate lines.
     def create_display_labels(self):
         sum_label = tk.Label(self.display_frame, text=self.sum_expression, anchor=tk.E,
-                             bg=LIGHT_GRAY, fg=LABEL_COLOUR,padx=24, font=SMALL_FONT)
+                             bg=LIGHT_GRAY, fg=LABEL_COLOUR, padx=24, font=SMALL_FONT)
         sum_label.grid(sticky="e")
 
         label = tk.Label(self.display_frame, text=self.current_expression,
-                             bg=LIGHT_GRAY, fg=LABEL_COLOUR,padx=24, font=LARGE_FONT)
+                         bg=LIGHT_GRAY, fg=LABEL_COLOUR, padx=24, font=LARGE_FONT)
         label.grid(sticky="e")
-
-        return(sum_label,label)
+        return sum_label, label
 
     # creates operator buttons.
     def create_operator_buttons(self):
         i = 0
         for operator, symbol in self.operations.items():
             button = tk.Button(self.buttons_frame, text=symbol, highlightbackground=CREAM,
-                               fg=LABEL_COLOUR, font=DIGITS_FONT,borderwidth=1)
+                               fg=LABEL_COLOUR, font=DIGITS_FONT, borderwidth=1, command=lambda i=symbol: self.add_to_label(i))
             button.grid(row=i, column=4, sticky=tk.NSEW)
             i += 1
 
@@ -106,22 +107,38 @@ class Calculator:
     def create_digit_buttons(self):
         for digit, grid_value in self.digits.items():
             button = tk.Button(self.buttons_frame, text=str(digit), highlightbackground=WHITE,
-                               fg=LABEL_COLOUR, font=DIGITS_FONT,borderwidth=1)
+                               fg=LABEL_COLOUR, font=DIGITS_FONT, borderwidth=1, command=lambda i=digit: self.add_to_label(i))
             button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
 
     # creates clear button
     def create_clear_button(self):
         button = tk.Button(self.buttons_frame, text="C", highlightbackground=WHITE,
-                           fg=LABEL_COLOUR, font=DIGITS_FONT,borderwidth=1)
+                           fg=LABEL_COLOUR, font=DIGITS_FONT, borderwidth=1, command=lambda: self.clear_button_true())
         button.grid(row=0, column=1, columnspan=3, sticky=tk.NSEW)
 
     # creates equals button
     def create_equals_button(self):
         button = tk.Button(self.buttons_frame, text="=", highlightbackground=LIGHT_BLUE,
-                           fg=LABEL_COLOUR, font=DIGITS_FONT,borderwidth=0)
+                           fg=LABEL_COLOUR, font=DIGITS_FONT, borderwidth=0, command=lambda: self.equals_button_true())
         button.grid(row=4, column=3, columnspan=2, sticky=tk.NSEW)
 
-root = tk.Tk() # creating window
-calculator = Calculator(root) # creating object of class by passing window.
-root.mainloop()
+    def equals_button_true(self):
+        self.sum_expression = self.current_expression
+        self.current_expression=""
+        self.update_label()
+        self.update_sum_label()
 
+    def clear_button_true(self):
+        self.current_expression=""
+        self.sum_expression=""
+        self.update_label()
+        self.update_sum_label()
+    def update_sum_label(self):
+        self.sum_label.config(text=self.sum_expression)
+
+    def update_label(self):
+        self.label.config(text=self.current_expression)
+
+root = tk.Tk()  # creating window
+calculator = Calculator(root)  # creating object of class by passing window.
+root.mainloop()
